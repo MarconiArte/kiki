aae095f6f618c20fcd52e15831fa56de60df9ba8
Marcos Niz <marconi4305@gmail.com>
2022-08-08 19:45:23 -0300

    Se modifico el CSS del proyecto

--- JS/app.js
+++ JS/app.js
@@ -11,11 +11,11 @@ class Capitulo {
 
 //------- variables (objetos y arrays)-------------
 
-const capituloUno = new Capitulo ("-----Capitulo 1-----\nKiki es una chica de 19 años un tanto rara\nNo tiene amigos y le gusta pasar tiempo en su cuarto a solas\n", "Kiki decidio escuchar música acostada en su cama","Ella es muy indecisa lo cual debes ayudarla a escoger una canción\n1)Cancion Uno\n2)Cancion Dos \n3)Cancion Tres",2 ,["¡¡¡QUE CANCIÓN DE MIERDA!!!", "Me gusta esta canción",`¿Podes elegir mejor chabón?\nPor favor te lo pido.`])
+const capituloUno = new Capitulo ("-----Capitulo 1-----\nKiki es una chica de 19 años un tanto rara\nNo tiene amigos y le gusta pasar tiempo en su cuarto a solas\n", "Kiki decidio escuchar música acostada en su cama","Ella es muy indecisa lo cual debes ayudarla a escoger una canción\n1)Cancion Uno\n2)Cancion Dos \n3)Cancion Tres",2 ,['"¡¡¡QUE CANCIÓN DE MIERDA!!!"', '"Me gusta esta canción"',`"¿Podes elegir mejor chabón?\nPor favor te lo pido."`])
 
-const capituloDos = new Capitulo ("CAPITULO DOS","Kiki recuerda que es hora de tomar su medicacíon pero olvido cual era...","Tiene tres tipos de pastillas la cual ella identifica por colores\n1)Roja\n2)Azul\n3)Verde",3 ,["La roja la tome esta mañana", "Azul... creo que es para dormir","Mmmmm...tomare la verde, supongo que es esta"])
+const capituloDos = new Capitulo ('CAPITULO DOS','Kiki recuerda que es hora de tomar su medicacíon pero olvido cual era...','Tiene tres tipos de pastillas la cual ella identifica por colores\n1)Roja\n2)Azul\n3)Verde',3 ,['"La roja la tome esta mañana"', '"Azul... creo que es para dormir"','"Mmmmm...tomare la verde, supongo que es esta"'])
 
-const capituloTres = new Capitulo ("CAPITULO TRES", "Kiki tiene que ir a la cocina por un poco de agua para tomar su medicación\nAl llegar a la cocina se percata de que algo la esta mirando","Al voltear y ver quien la observa se encuentra con una criatura humanoide llena de bocas con dientes filosos\nEntre susto y desesperacion busca algun objeto para defenderse\n1)Un palo para amasar que uso la madre\n2)Un cuchillo\n3)Una sarten sobre la mesada",1 ,["Kiki agarra el palo de amasar, golpea a la criatura y corre", "Kiki agarra el cuchillo se lanza hacia la criatura pero esta esquiva su ataque y le perfora el estomago con su brazo","Kiki agarra la sarten pero le parecia muy pesada para manipular y la criatura se le tira encima matandola"])
+const capituloTres = new Capitulo ('CAPITULO TRES', 'Kiki tiene que ir a la cocina por un poco de agua para tomar su medicación\nAl llegar a la cocina se percata de que algo la esta mirando','Al voltear y ver quien la observa se encuentra con una criatura humanoide llena de bocas con dientes filosos\nEntre susto y desesperacion busca algun objeto para defenderse\n1)Un palo para amasar que uso la madre\n2)Un cuchillo\n3)Una sarten sobre la mesada',1 ,['"Kiki agarra el palo de amasar, golpea a la criatura y corre"', '"Kiki agarra el cuchillo se lanza hacia la criatura pero esta esquiva su ataque y le perfora el estomago con su brazo"','"Kiki agarra la sarten pero le parecia muy pesada para manipular y la criatura se le tira encima matandola"'])
 
 const capitulos = [capituloUno, capituloDos, capituloTres]
 
@@ -58,9 +58,8 @@ function opciones(capitulos) {
     })
 }
 
-
-
 let capituloActual = 0
+
 siguienteCap.addEventListener('click', () => {
     siguienteCap.style.display = "none"
 
