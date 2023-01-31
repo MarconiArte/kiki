30488f2e55aa6362f07e724c36b677a441b21daf
Marcos Niz <marconi4305@gmail.com>
2023-01-31 15:14:17 -0300

    se termino el proyecto

--- JS/app.js
+++ JS/app.js
@@ -1,59 +1,3 @@
-//--------------------------Clases-----------------------------
-class Capitulo {
-    constructor(titulo, resumen,imagen, pregunta, correcta,respuestas) {
-        this.titulo = titulo
-        this.resumen = resumen
-        this.imagen = imagen
-        this.pregunta = pregunta //Opciones a elegir
-        this.correcta = correcta //numerito
-        this.respuestas = respuestas //Array de respuestas segun la pregunta
-    }
-}
-
-//----------variables (objetos y arrays)-------------
-
-const capituloUno = new Capitulo ("Kiki es una chica de 19 años un tanto rara, no tiene amigos y le gusta pasar tiempo en su cuarto a solas", "Kiki decidio escuchar música acostada en su cama","./img/cap1part1.jpg","Ella es muy indecisa lo cual debes ayudarla a escoger una canción 1)Cancion Uno 2)Cancion Dos  3)Cancion Tres",2 ,['"¡¡¡NO ME GUSTA ESTA CANCIÓN!!!"', '"Me gusta esta canción"',`"¿Podes elegir mejor? Por favor te lo pido."`])
-
-const capituloDos = new Capitulo ('CAPITULO DOS',"Kiki recuerda que es hora de tomar su medicacíon pero olvido cual era...",'./img/cap1part1.jpg','Tiene tres tipos de pastillas la cual ella identifica por colores 1)Roja 2)Azul 3)Verde',3 ,['"La roja la tome esta mañana"', '"Azul... creo que es para dormir"','"Mmmmm...tomare la verde, supongo que es esta"'])
-
-const capituloTres = new Capitulo ('CAPITULO TRES',"Kiki tiene que ir a la cocina por un poco de agua para tomar su medicación. Al llegar a la cocina se percata de que algo la esta mirando",'./img/cap1part1.jpg','Al voltear y ver quien la observa se encuentra con una criatura humanoide llena de bocas con dientes filosos Entre susto y desesperacion busca algun objeto para defenderse 1)Un palo para amasar que uso la madre 2)Un cuchillo 3)Una sarten sobre la mesada',1 ,['"Kiki agarra el palo de amasar, golpea a la criatura y corre"', '"Kiki agarra el cuchillo se lanza hacia la criatura pero esta esquiva su ataque y le perfora el estomago con su brazo"','"Kiki agarra la sarten pero le parecia muy pesada para manipular y la criatura se le tira encima matandola"'])
-
-const capitulos = [capituloUno, capituloDos, capituloTres]
-
-const siguienteCap = document.getElementById("siguienteCap")
-
-let capituloActual = 0
-
-//------------FUNCIONES------------------
-
-function presentacionCapitulo(capitulo) {
-
-    const divCapitulos = document.getElementById("divCap")
-    
-    divCapitulos.innerHTML = `
-    <h2 class="bordeRojo">${capitulo.titulo}</h2>
-    <p>${capitulo.resumen}</p>
-    <img src="${capitulo.imagen}">
-    <p>${capitulo.pregunta}</p>
-    `  
-}
-
-//--------------EVENTOS----------------------
-
-siguienteCap.addEventListener('click', () => {
-    siguienteCap.style.display = "none"
-
-    document.getElementById("divRespuesta").innerHTML = ``
-
-    if (capituloActual < capitulos.length) {
-        
-        capituloActual++
-
-        presentacionCapitulo(capitulos[capituloActual])
-
-    }    
-})
-
 //-----------BIENVENIDO USER--------------
 
 const arrayNick = []
@@ -88,10 +32,56 @@ if (localStorage.getItem('jugadores')) {
     
     })
 }
+//-----------------Globales---------------------
 
+const siguienteCap = document.getElementById("siguienteCap")
+
+let capituloActual = 0 
+
+
+//------------FUNCIONES------------------
+
+function presentarCapJson () {
+
+    fetch("./json/capitulosJson.json")
+
+    .then(response => response.json())
+
+    .then(capitulosJson => {
+        const divCapitulos = document.getElementById("divCap")
+        
+        divCapitulos.innerHTML = `
+            <h2 class="bordeRojo">${capitulosJson[capituloActual]["titulo"]}</h2>
+            <p>${capitulosJson[capituloActual]["resumen"]}</p>
+            <img src="${capitulosJson[capituloActual]["imagen"]}">
+            <p>${capitulosJson[capituloActual]["pregunta"]}</p>
+        ` 
+})
+}
+
+//--------------EVENTOS----------------------
+
+siguienteCap.addEventListener('click', () => {
+    siguienteCap.style.display = "none"
+    
+    document.getElementById("divRespuesta").innerHTML = ``
+
+    fetch("./json/capitulosJson.json")
+
+    .then(response => response.json())
+
+    .then(capitulosJson =>{
+        if (capituloActual < capitulosJson.length) {
+        
+            capituloActual++
+    
+            presentarCapJson(capitulosJson[capituloActual])
+        }    
+
+    })
+})
 
 //-----------PROGRAMA PRINCIPAL-----------
-
 const formulario = document.getElementById("form3")
 
 formulario.addEventListener("submit", (e) => {
@@ -104,24 +94,20 @@ formulario.addEventListener("submit", (e) => {
 
     let opcionSelect = respuestas.options[respuestas.selectedIndex].value
     
-    console.log(opcionSelect)
+    fetch("./json/capitulosJson.json")
 
-    divRespuesta.innerHTML = `
-            <p>${capitulos[capituloActual].respuestas[opcionSelect-1]} </p>
+    .then(response => response.json())
+
+    .then(capitulosJson => {
+        divRespuesta.innerHTML = `
+            <p>${capitulosJson[capituloActual]["respuesta"][opcionSelect-1]} </p>
         `
-    if (opcionSelect == capitulos[capituloActual].correcta) {
-        siguienteCap.style.display = "block"
-    }
+        if (opcionSelect == capitulosJson[capituloActual]["correcta"]) {
+            siguienteCap.style.display = "block"
+        }
+    })
 })
 
-presentacionCapitulo(capitulos[capituloActual])
+presentarCapJson()
 
 
-//--------JSON y FECTH--------------
-fetch("./json/capitulosJson.json")
-
-.then(response => response.json())
-
-.then(capitulosJson => {
-    console.log(capitulosJson)
-})
